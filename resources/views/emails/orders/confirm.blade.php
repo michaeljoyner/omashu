c7c3684b644a611219b487d8b96e5415c00f1fd8
Michael Joyner <joyner.michael@gmail.com>
2016-03-31 08:35:43 +0800

    correct email addresses

--- resources/views/emails/orders/confirm.blade.php
+++ resources/views/emails/orders/confirm.blade.php
@@ -3,7 +3,7 @@
 
 <p>
     當您的包裹寄出時，我們會寄送一封含有連結的電子郵件來追蹤訂單。<br>
-    如果您對訂單有任何問題，請聯絡：contacts@omashuimports.com 或致電：04-25222019。<br>
+    如果您對訂單有任何問題，請聯絡：contact@omashuimports.com 或致電：04-25222019。<br>
     您的訂單確認如下，再次謝謝您的惠顧。
 </p>
 <p style="font-size: 1.2em; color: #428D90;">訂單號碼#{{ $order_number }}</p>
@@ -24,7 +24,7 @@
     請於三日內匯款<br>
     超過三日一律取消訂單<br>
     當您完成匯款時<br>
-    請透過電子郵件 (contact@omashuimport.com)聯絡我們<br>
+    請透過電子郵件 (contact@omashuimports.com)聯絡我們<br>
     通知匯款完成時請提供<br>
     1.訂單編號或收件人姓名<br>
     2.匯款帳號後五碼及金額，以便確認您的款項，也請保存您的匯款收據與訂單號碼，以便日後查詢。<br>
